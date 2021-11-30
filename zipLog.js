97be9839228684f4c7e4031ea66ba7f011ef0ba3
mattpaynedev <72278561+mattpaynedev@users.noreply.github.com>
2021-11-30 11:22:19 -0700

    Refactor list structure Create ranges on Display call

--- zipLog.js
+++ zipLog.js
@@ -1,7 +1,6 @@
 class ZipNode {
     constructor(val, prev = null, next = null) {
-        this.start = val
-        this.end = val
+        this.val = val
         this.prev = prev
         this.next = next
     }
@@ -27,11 +26,15 @@ exports.ZipLog = class ZipLog {
         const arr = []
         let node = this.list
         while (node) {
-            if (node.start === node.end) {
-                arr.push(this._stringifyZip(node.start))
-            } else {
-                arr.push(this._stringifyZip(node.start) + "-" + this._stringifyZip(node.end))
+            const start = node.val
+            while (node.next && node.next.val === node.val + 1) {
+                node = node.next
             }
+            const end = node.val
+            arr.push(start === end
+                ? this._stringifyZip(start)
+                : this._stringifyZip(start) + "-" + this._stringifyZip(end)
+            )
             node = node.next
         }
         return {
@@ -77,25 +80,23 @@ exports.ZipLog = class ZipLog {
         } else {
             let node = this.list
             let prev = null
-            while (node && node.start < val) {
+            while (node && node.val < val) {
                 // find the insertion point
                 prev = node
                 node = node.next
             }
-            if (!prev) {
-                this._insertAtBeginning(prev, node, val)
-            } else if (!node) {
-                this._insertAtEnd(prev, node, val)
-            } else if (node.start > val + 1 && prev.end < val - 1) {
-                const newNode = new ZipNode(val)
-                this._insertNewNode(prev, node, newNode)
-                this.codes[val] = newNode
-            } else if (prev.end === val - 1) {
-                this._mergeWithPrevious(prev, node, val)
-            } else if (node.start === val + 1) {
-                this._mergeWithNext(node, val)
+            const newNode = new ZipNode(val)
+            if (prev) {
+                prev.next = newNode
+            } else {
+                this.list = newNode
             }
+            newNode.prev = prev
+            newNode.next = node
+            if (node) node.prev = newNode
+            this.codes[val] = newNode
         }
+
         return {
             success: true,
             message: "Zip Code inserted",
@@ -103,57 +104,6 @@ exports.ZipLog = class ZipLog {
                 inserted: this._stringifyZip(val)
             }
         }
-    }
-
-    _insertNewNode(prev, node, newNode) {
-        if (prev) prev.next = newNode
-        newNode.prev = prev
-        newNode.next = node
-        if (node) node.prev = newNode
-    }
-
-    _insertAtBeginning(prev, node, val) {
-        if (node.start > val + 1) {
-            const newNode = new ZipNode(val)
-            this._insertNewNode(prev, node, newNode)
-            this.list = newNode
-            this.codes[val] = newNode
-        } else {
-            node.start = val
-            this.codes[val] = node
-        }
-    }
-
-    _insertAtEnd(prev, node, val) {
-        if (prev.end === val - 1) {
-            prev.end = val
-            this.codes[val] = prev
-        } else {
-            const newNode = new ZipNode(val)
-            this._insertNewNode(prev, node, newNode)
-            this.codes[val] = newNode
-        }
-    }
-
-    _mergeWithPrevious(prev, node, val) {
-        prev.end = val
-        if (node.start === val + 1) {
-            // merge with next node, if needed
-            prev.end = node.end
-            let n = node.start
-            while (n < node.end) {
-                this.codes[n] = prev
-                n++
-            }
-            if (node.next) node.next.prev = prev
-            prev.next = node.next
-        }
-        this.codes[val] = prev
-    }
-
-    _mergeWithNext(node, val) {
-        node.start = val
-        this.codes[val] = node
     }
 
     delete(val) {
@@ -165,26 +115,14 @@ exports.ZipLog = class ZipLog {
         }
 
         const node = this.codes[val]
-        if (node.start === val && node.end === val) {
-            node.prev.next = node.next
-            node.next.prev = node.prev
-        } else if (node.start === val) {
-            node.start = val + 1
-        } else if (node.end === val) {
-            node.end = val - 1
+        if (!node.prev) {
+            // move the head pointer
+            this.list = node.next
         } else {
-            const newNode = new ZipNode(val + 1)
-            newNode.end = node.end
-            newNode.prev = node
-            newNode.next = node.next
-            let n = newNode.start
-            while (n < newNode.end) {
-                this.codes[n] = newNode
-                n++
-            }
-            node.end = val - 1
-            node.next = newNode
+            node.prev.next = node.next
         }
+        if (node.next) node.next.prev = node.prev
+
         delete this.codes[val]
         return {
             success: true,
